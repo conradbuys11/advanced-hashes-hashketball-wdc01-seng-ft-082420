b718e97681ff102b15ec06baac323bc6ffe91095
Conrad Buys <conradbuys11@gmail.com>
2020-08-13 18:37:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,7 +146,7 @@ end
 
 def team_colors(team_name)
   game_hash.each do |team, categories|
-    if team[:team_name] == team_name
+    if categories[:team_name] == team_name
       return team[:colors]
     end
   end
